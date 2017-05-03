63657d6e4669afa2d84497e71105bf2f36f61506
Mike Paige <mdpaige@gmail.com>
2017-05-03 22:02:32 +0000

    added some links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,19 @@
 class ArticlesController < ApplicationController
   
   def new
+    @article = Article.new
   end
   
   def show
     @article = Article.find(params[:id])
   end
+ 
+  def edit
+    @article = Article.find(params[:id])
+  end
   
   def index
     @articles = Article.all
-  end
-  
-  
-  def create
-    render plain: params[:article].inspect
   end
   
   def create
@@ -24,6 +24,17 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+  
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   
   private
   def article_params
